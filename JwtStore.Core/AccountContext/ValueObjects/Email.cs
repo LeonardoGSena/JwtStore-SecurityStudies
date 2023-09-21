0192fb2d42e586d638590234aeed218b46ef46ba
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-09-21 17:03:57 -0300

    Finished Verification value object

---
+++ JwtStore.Core/AccountContext/ValueObjects/Email.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.RegularExpressions;
+using JwtStore.Core.SharedContext.Extensions;
+using JwtStore.Core.SharedContext.ValueObjects;
+
+namespace JwtStore.Core.AccountContext.ValueObjects;
+
+public partial class Email : ValueObject
+{
+    private const string Pattern = @"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$";
+
+    public Email(string address)
+    {
+        if (string.IsNullOrEmpty(address))
+            throw new Exception("E-mail inválido");
+
+        Address = address.Trim().ToLower();
+
+        if (Address.Length < 5)
+            throw new Exception("E-mail Inválido");
+
+        if (!EmailRegex().IsMatch(Address))
+            throw new Exception("E-mail Inválido");
+    }
+
+    public string Address { get; }
+    public string Hash => Address.ToBase64(); // Gravatar.com
+
+    public static implicit operator string(Email email)
+        => email.ToString();
+
+    public static implicit operator Email(string address)
+        => new Email(address);
+
+    public override string ToString()
+        => Address.Trim().ToLower();
+
+    [GeneratedRegex(Pattern)]
+    private static partial Regex EmailRegex();
+}
