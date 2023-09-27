1bc0d15f53c50fc9e20d428d959e18b82b37e180
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-09-27 14:16:13 -0300

    Add user-secrets

--- JwtStore.Api/obj/Debug/net7.0/JwtStore.Api.AssemblyInfo.cs
+++ JwtStore.Api/obj/Debug/net7.0/JwtStore.Api.AssemblyInfo.cs
@@ -10,6 +10,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("c7a59b44-9068-4393-9066-9ce8b191c4da")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("JwtStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
