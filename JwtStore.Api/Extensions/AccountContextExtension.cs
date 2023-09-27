e30dc0799e696069dd94060c52b3ab28b10b3efa
LeonardoGSena <lgomesgabriel@hotmail.com>
2023-09-27 13:47:35 -0300

    Registrin dependencies

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -1,9 +1,16 @@
-﻿namespace JwtStore.Api.Extensions;
+﻿using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using JwtStore.Infra.Contexts.AccountContext.UseCases.Create;
+
+namespace JwtStore.Api.Extensions;
 
 public static class AccountContextExtension
 {
     public static void AddAccountContext(this WebApplicationBuilder builder)
     {
+        #region Create
+        builder.Services.AddTransient<IRepository, Repository>();
+        builder.Services.AddTransient<IService, Service>();
+        #endregion
 
     }
 
